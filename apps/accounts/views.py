1c342a1da12e8a72a6d1d79dc4df122008115132
rogeriodelphi <rogeriodelphi@gmail.com>
2021-10-22 23:24:58 -0300

    Criado perfil de usuário

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import UserForm
+from .forms import UserForm, UserProfileForm
 
 
 def add_user(request):
@@ -49,6 +49,22 @@ def user_login(request):
         else:
             messages.error(request, "Usuário ou senha inválidos")
     return render(request, template_name, {})
+
+
+@login_required(login_url='/contas/login/')
+def add_user_profile(request):
+    template_name = 'accounts/add_user_profile.html'
+    context = {}
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.user = request.user
+            f.save()
+            messages.success(request, 'Perfil alterado com sucesso!')
+    form = UserProfileForm()
+    context['form'] = form
+    return render(request, template_name, context)
 
 
 @login_required(login_url='/contas/login/')
